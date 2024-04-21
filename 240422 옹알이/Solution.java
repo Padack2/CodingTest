aa23ad7b2a1af89bd126b400b0820a634782fe45
Padack2 <ajuma777@naver.com>
2024-04-22 08:41:25 +0900

    0422 옹알이

--- 240422 옹알이/Solution.java
+++ 240422 옹알이/Solution.java
@@ -3,8 +3,10 @@ class Solution {
         int answer = 0;
         
         for (String str : babbling) {
-            str = str.replaceAll("ayaaya|yeye|woowoo|mama", " ");
-            if (str.replaceAll("aya|ye|woo|ma", "").equals("")) {
+            str = str.replaceAll("ayaaya|yeye|woowoo|mama", " ")
+                    .replaceAll("aya|ye|woo|ma", "");
+            
+            if (str.equals("")) {
                 answer++;
             }
         }
