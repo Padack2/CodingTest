d13d531df499abee17a244f44676aeb8edde7922
Padack2 <ajuma777@naver.com>
2024-05-12 08:57:38 +0900

    0512 둘만의 암호

---
+++ 240512 둘만의 암호/Soluion.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s, String skip, int index) {      
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            for (int j = 0; j < index; j++) {
+                c ++;
+                
+                if (c > 'z') {
+                    c -= 26;
+                }
+                if (skip.indexOf(c) >= 0) {
+                    j --;
+                }
+            }
+            
+            sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}
